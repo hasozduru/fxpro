458cadb39cf9a5085416fc09bc61a06e166c63d3
Hasan Ozduru <hasozduru@yahoo.com>
2011-06-27 00:01:01 +0100

    Test6

--- js.js
+++ js.js
@@ -2,7 +2,6 @@ $(document).ready(function(){
 	$("#paging_button li").click(function(){
 		$("#paging_button li").css({'background-color' : ''});
 		$(this).css({'background-color' : '#A5CDFA'});
-
 		$("#content_ajax").load("data.php?page=" + this.id);
 	});
 	// by default first time this will execute
